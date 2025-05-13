25ed25908dc5a026d864257622aa77473de64a66
david-why <david_why@outlook.com>
2025-05-13 15:41:13 +0800

    Initial Commit

---
+++ Calendarize/CalendarizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarizeApp.swift
+//  Calendarize
+//
+//  Created by David Wang on 2025/5/13.
+//
+
+import SwiftUI
+
+@main
+struct CalendarizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
